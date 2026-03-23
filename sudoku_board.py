af9f92b1f9e3f43932c07757852138d6f492af55
Hiromi Nurse <90805655+HiromiNurse@users.noreply.github.com>
2026-03-23 18:59:40 -0500

    Sudoku_board.py update
    Updated sudoku board to have some printing functionality for sudoku objects.

--- sudoku_board.py
+++ sudoku_board.py
@@ -8,17 +8,33 @@ class Sudoku:
 
     def __str__(self) -> str:
         s = ""
-        # YOUR CODE HERE
+        # Printing a str of sudoku as text in 9x9 grid format
+        for j in range(1, 10):
+            for i in range(1, 10):
+                s += str(self.sudoku[i, j]) + " "
+            s += "\n"
         return s
 
     @classmethod
     def from_str(cls, s: str) -> "Sudoku":
         sudoku = {}
-        # YOUR CODE HERE
+        # Create a sudoku class object from a 9x9 string representation of a sudoku puzzle
+        lines = s.strip().splitlines()
+        for j, line in enumerate(lines, start=1):
+            for i, char in enumerate(line.strip().split(), start=1):
+                sudoku[i, j] = int(char)
         return cls(sudoku)
 
     @classmethod
     def from_model(cls, model: clingo.solving.Model) -> "Sudoku":
         sudoku = {}
-        # YOUR CODE HERE
+        # Create a sudoku class object from a clingo model
+        # Look at this code. Not sure how good it is.
+        for atom in model:
+            if atom.name == "cell" and len(atom.arguments) == 3:
+                i = atom.arguments[0].number
+                j = atom.arguments[1].number
+                value = atom.arguments[2].number
+                sudoku[i, j] = value
         return cls(sudoku)
+
