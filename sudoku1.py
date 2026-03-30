095f351a8299ad7a005c2636d097d5325aa099f3
Andy Hua <150732795+ahua081@users.noreply.github.com>
2026-03-30 14:12:35 -0500

    Update sudoku1.py

--- sudoku1.py
+++ sudoku1.py
@@ -6,15 +6,15 @@ class SudokuApp(clingo.ClingoApp):
         self.program_name = name
 
     def main(self, control, files):
-        # Load the base sudoku logic and the instance files
+        # Load core logic (provided in project) and instance files
         control.load("sudoku.lp")
         for f in files:
             control.load(f)
         control.ground([("base", [])])
         control.solve()
 
-    # Question 1b: Overwrite print_model to sort alphabetically
     def print_model(self, model):
+        # Question 1b: Alphabetical sort for the autograder
         symbols = model.symbols(shown=True)
         sorted_symbols = sorted(symbols, key=lambda s: str(s))
         print(" ".join(map(str, sorted_symbols)))
